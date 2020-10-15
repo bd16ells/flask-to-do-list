5b2c6a51d084fb788d6b1c207a12236ce3d7e240
Benjamin Ellsworth <bd16ells@siena.edu>
2020-10-15 09:01:30 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 # insert path below
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////absolute/path/to/database.db'
+MY_PATH = "your/path/to/flask-to-do-list"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{MY_PATH}/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
